68eaec8533d063778a1248782d3101dc5c495773
dangthuy112 <47605433+dangthuy112@users.noreply.github.com>
2021-09-26 03:04:42 -0500

    adminhomepage updated v0.5
    prototype of the admin homepage.

---
+++ Gradebook/adminhomepage.php
@@ -0,0 +1,35 @@
+<html> 
+    <head>
+        <title>Admin Homepage </title>
+
+        <link rel="stylesheet" href="assets/css/adminhomepage.css">
+    </head>
+
+    <body>
+        <!-- menu section starts-->
+        <div class="menu">
+            <div class="wrapper">
+                Menu Here
+            </div>
+        </div>
+        <!-- menu section ends-->
+
+        <!-- admin manage section-->
+        <div class="admin-manage">
+            <div class="wrapper">
+                Menu Here
+            </div>
+        </div>
+        <!-- admin manage section ends-->
+
+        <!-- menu section starts-->
+        <div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2021 All rights reserved, Some School, Developped by Squad 3</p>
+            </div>
+        </div>
+        <!-- menu section ends-->
+
+    </body>
+
+</html>
