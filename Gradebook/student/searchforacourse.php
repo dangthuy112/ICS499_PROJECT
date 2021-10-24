7d55cd15c2c83e3b3a2ef00494e09214d084a46f
trungpham7890 <trungpham7890@gmail.com>
2021-10-23 21:16:14 -0500

    minor chage
    minor change

--- Gradebook/student/searchforacourse.php
+++ Gradebook/student/searchforacourse.php
@@ -1,39 +1,143 @@
-<link rel="stylesheet" href="assets/css/styles.css">
+<link rel="stylesheet" href="assets/css/searchforacourse.css">
+
 <?php
-//static $sid = 9;
-// static $logoutlink="logout.php";
-$sid=$_GET['sid'];
-$connection_string = 'mysql:host=localhost:3307;dbname=gradebook1';
-$user_name = 'student'; //testing
-$password = 'trungbasau123';
-$connection = mysqli_connect("localhost:3307", "student", "trungbasau123", "gradebook1");
+$sid = $_GET['sid'];
+$connection = mysqli_connect("localhost:3307", "student", "trungbasau123", "studentgradebook");
 if ($connection->connect_error) {
   die("Connection Failed:" . $connection->connect_error);
+}
+$sql = "SELECT courses.subject  FROM courses GROUP BY courses.subject HAVING COUNT(*)>1";
+$result = $connection->query($sql);
+$subject = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $subject[] = $row['subject'];
 }
 include('studentheader.php');
 include('studentmenu.php');
 ?>
-<head></head>  
-<title>Static Dropdown List</title>  
-<div class ="padtable">
-  <div class ="middlediv">
-    <b>Seach For A Course </b>
-<select>  
-  <option value="Select">Subjects</option> 
-  <option value="Vineet">Vineet Saini</option>  
-  <option value="Sumit">Sumit Sharma</option>  
-  <option value="Dorilal">Dorilal Agarwal</option>  
-  <option value="Omveer">Omveer Singh</option>  
-  <option value="Rohtash">Rohtash Kumar</option>  
-  <option value="Maneesh">Maneesh Tewatia</option>  
-  <option value="Priyanka">Priyanka Sachan</option>  
-  <option value="Neha">Neha Saini</option>  
-</select>   
-<button>Search</button>
+
+<div class="padtable">
+  <div class="middlediv">
+    <form action="" method="POST">
+      <table >
+        <tr>
+          <th>
+            <div class="divcenter">
+              <b>Seach For A Course </b>
+            </div>
+          </th>
+        </tr>
+        <tr>
+          <td><b>Subject</b></td>
+          <td><select name="subject">
+              <option value="Select"></option>
+              <?php
+              for ($x = 0; $x < count($subject); $x++) {
+                echo "<option >$subject[$x]</option>";
+              }
+              ?>
+              <option value="Any">Any</option>
+            </select>
+          </td>
+        </tr>
+
+        <tr>
+          <td><b>Semester</b></td>
+          <td>
+            <select name="semester">
+              <option value="Select"></option>
+              <option value="Current Semester">Current Semester</option>
+              <option value="Next Semester">Next Semester</option>
+              <option value="Any">Any</option>
+            </select>
+          </td>
+          <br>
+        </tr>
+        <tr>
+          <th>
+          <td>
+            <div class="divcenter"><input type="submit" name="submit" value="Search"></div>
+          </td>
+          </th>
+        </tr>
+      </table>
+    </form>
+  </div>
+ 
+  <?php
+  if (isset($_POST['submit'])) {
+    $sem = $_POST['semester'];
+    $sbj = $_POST['subject'];
+    $sql = "SELECT* FROM courses WHERE courses.subject='$sbj' and courses.semester='$sem'";
+  if($sem=="Any"&&$sbj!="Any")
+  {
+    $sql = "SELECT* FROM courses WHERE courses.subject='$sbj'";
+  }
+  elseif($sem!="Any"&&$sbj=="Any")
+  {
+    $sql = "SELECT* FROM courses WHERE courses.semester='$sem'";
+  }
+  elseif($sem=="Any"&&$sbj=="Any")
+  {
+    $sql = "SELECT* FROM courses";
+  }
+ 
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+      echo "
+        <table style='border= 1px solid black;margin-left: auto; margin-right: auto;'>
+          <tr>
+            <th style='border: 2px solid black'>CouseID</th>
+            <th style='border: 2px solid black'>Subject</th>
+            <th style='border: 2px solid black'>Couse Number</th>
+            <th style='border: 2px solid black'>Semester</th>
+            <th style='border: 2px solid black'>Days</th>
+            <th style='border: 2px solid black'>Location</th>
+            <th style='border: 2px solid black'>Delivery Method</th>
+            <th style='border: 2px solid black'>Time</th>
+            <th style='border: 2px solid black'>Intructor</th>
+            <th style='border: 2px solid black'>Course Name</th>
+          </tr>";
+      // output data of each row
+      echo $result->num_rows;
+      while ($row = mysqli_fetch_assoc($result)) {
+        $id = $row['courseID'];
+        $courseid = strval($id);
+        echo "<tr>
+        <td style='border: 2px solid black'>"
+          . $row["courseID"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["subject"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["coursenumber"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["semester"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["days"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["location"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["delivery method"] . "</td><td style='border:2px solid black'>"
+
+          . $row["time"] . "</td><td style='border: 2px solid black'>"
+
+          . $row["Instructor"] . "</td><td style='border: 2px solid black'><a  href='./coursedetail.php?sid=$sid&courseid=$courseid'>"
+
+
+          .$row["coursename"] . "</a></td></tr>";
+      }
+      echo "</table>";
+    } else {
+      echo "0 results";
+    }
+  }
+  ?>
+
 </div>
-</div> 
- <?php
+
+</div>
+<?php
 $connection->close();
-include('studentfooter.php') ;
+include('studentfooter.php');
+
 ?>
-       
--- Gradebook/student/studentpage.php
+++ Gradebook/student/studentpage.php
@@ -1,33 +1,60 @@
-<link rel="stylesheet" href="assets/css/styles.css">
+
+<!-- <style>
+.padtable{
+  height: 70%;
+  width: 100%;
+  border: 1px solid black;
+  display: inline-block;
+  background-color: lightpink;
+
+}
+.middlediv{
+  margin-left: 40%;
+  height: fit-content;
+  width: fit-content;
+  border: 1px solid;
+  padding: 10px;
+  box-shadow: 5px 10px #888888;
+
+}
+</style> -->
 <?php
 $sid = 9; //try student id constant
 include('studentheader.php');
 include('studentmenu.php');
-$connection_string = 'mysql:host=localhost:3307;dbname=gradebook1';
-$user_name = 'student'; //testing
-$password = 'trungbasau123';
-$db = new PDO($connection_string, $user_name, $password);
+// $connection_string = 'mysql:host=localhost:3307;dbname=gradebook1';
+// $user_name = 'student'; //testing
+// $password = 'trungbasau123';
+// $db = new PDO($connection_string, $user_name, $password);
+$connection = mysqli_connect("localhost:3307", "student", "trungbasau123", "studentgradebook");
+if ($connection-> connect_error) {
+    die("Connection Failed:". $connection-> connect_error);
+}
+
+$sql = "SELECT students.fullname From students WHERE students.StudentID='$sid'";
+$result = $connection->query($sql);
+$subjectname = [];
+$row = mysqli_fetch_assoc($result);
+$studentname=$row['fullname'];
 ?>
 <link rel="stylesheet" href="assets/css/studentpage.css">
-
-
 <!-- admin manage section-->
 
 <div class="padtable">
   <b>ANNOUNCEMENT</b>
-  <table style="width:100%">
+  <table >
     <tr>
       <th>Date</th>
       <th>Announcement</th>
     </tr>
     <tr>
       <td>today</td>
-      <td>test1</td>
+      <?php echo "<td>Hello I am the student name $studentname and my student id is $sid</td>"?>;
     </tr>
-    <tr>
+    <!-- <tr>
       <td>tomorrow</td>
       <td>test2</td>
-    </tr>
+    </tr> -->
   </table>
 </div>
 <?php
