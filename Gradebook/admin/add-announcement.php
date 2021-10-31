65b2ad7592a6107d6442db298fd6a4a99f3c71dc
Aziz Moalim <43540550+azizmoalim@users.noreply.github.com>
2021-10-30 19:33:39 -0500

    Update add-announcement.php

--- Gradebook/admin/add-announcement.php
+++ Gradebook/admin/add-announcement.php
@@ -2,6 +2,16 @@
 
 $database = require 'bootstrap.php';
 
+if ((isset($_SESSION['username'])) && (isset($_SESSION['password']))) {
+    // This session already exists, should already contain data
+    # echo "User ID Username: ", $_SESSION['username'], "<br />";
+    # echo "User ID Password: ", $_SESSION['password'], "<br />";
+    # echo "User ID: ", $_SESSION['userID'], "<br />";
+} else {
+    // No Session Detected. Redirect to login page.
+
+    header("Location: ../login.php");
+}
 
 $courses = $database->selectAll("courses");
 
